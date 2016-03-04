e584a9dd4dba748fb3e33202d8ca1e99406dc115
A_Roques <mxtoinoo>
2016-03-04 10:54:21 +0100

    init project

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('oklmClient')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
